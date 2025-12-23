9743db371963b6cbb24e44605e16fab65d2dc676
DSW-robinsonruiz <101422223+DSW-robinsonruiz@users.noreply.github.com>
2025-12-23 00:32:11 -0500

    Update FiveM_Repair.cmd

--- FiveM_Repair.cmd
+++ FiveM_Repair.cmd
@@ -1,4 +1,4 @@
 @echo off
 title FiveM Repair Tool
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0FiveM_Repair.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/DSW-robinsonruiz/FiveM-Repair/main/FiveM_Repair.ps1 | iex"
 pause
